12d1afbb8982b80fc296402fa3f2f603ba17428f
Timon <timmwrite>
2019-07-17 18:20:17 +0300

    Arrays in python

---
+++ array1.py
@@ -0,0 +1,20 @@
+class Python_Array():
+    def __init__(self):
+        self.pythonarray = []
+        
+        
+    def add_list():
+        self.pythonarray.append()
+        
+    def insert_list():
+        self.pythonarray.insert()
+        
+    def remove_items():
+        self.pythonarray.remove()
+        
+    
+        
+        
+        
+        
+      
